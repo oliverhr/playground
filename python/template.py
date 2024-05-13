5e436058be98527664ea69c4ba0c960db9e1f96a
@OliverHR <oliverhdzrangel@gmail.com>
2024-05-12 20:10:12 -0600

    template added

---
+++ python/template.py
@@ -0,0 +1,12 @@
+def implementation():
+    pass
+
+
+def test():
+    expected = True
+    actual = False
+    assert expected == actual, f'expected {expected} but {actual} was received.'
+
+if __name__ == '__main__':
+    test()
+
