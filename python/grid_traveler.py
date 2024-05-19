5d3f6e32f7c194c8b5ca48bf3d161687b3f16494
OHR <@oliverhr>
2024-05-18 20:08:05 -0600

    Grid Traveler inner function

--- python/grid_traveler.py
+++ python/grid_traveler.py
@@ -1,9 +1,11 @@
-def grid_traveler(x, y, mem={}):
-    if x * y == 1: return 1
-    if x * y <= 0: return 0
-    if (id := f'{x},{y}') in mem: return mem[id]
-    mem[id] = grid_traveler(x-1, y) + grid_traveler(x, y-1)
-    return mem[id]
+def grid_traveler(m, n):
+    def fn(x, y, mem = {}):
+        if x * y == 1: return 1
+        if x * y <= 0: return 0
+        if (id := f'{x},{y}') in mem: return mem[id]
+        mem[id] = fn(x-1, y) + fn(x, y-1)
+        return mem[id]
+    return fn(m, n)
 
 
 if __name__ == '__main__':
